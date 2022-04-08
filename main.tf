f44111bebda6f4e1107317c09183d4608516268d
Naveen-manokaran <naveen.manokaran@bootlabstech.com>
2022-04-08 15:05:08 +0530

    initial

---
+++ main.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "web-server" {
+  ami                          =  var.ami
+  instance_type                = var.instance_type
+  associate_public_ip_address  = var.associate_public_ip_address
+  key_name                     = aws_key_pair.generated_key.key_name
+
+  root_block_device {
+    volume_size                = var.volume_size
+    delete_on_termination      = var.delete_on_termination
+  }
+
+  tags = {
+    Name = var.name
+  }
+  user_data = "${file("startup-script.sh")}"
+}
+
+resource "tls_private_key" "example" {
+  algorithm = var.algorithm
+  rsa_bits  = var.rsa_bits
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = var.key_name
+  public_key = tls_private_key.example.public_key_openssh
+}
---
+++ variables.tf
@@ -0,0 +1,58 @@
+variable "ami" {
+   description = "ID of AMI to use for the instance"
+   type        = string
+}
+
+variable "instance_type" {
+   description = "The type of instance "
+   type        = string
+}
+
+variable "associate_public_ip_address" {
+   description = "Whether to associate a public IP address with an instance in a VPC"
+   type        = bool
+}
+variable "volume_size" {
+   description = "Whether to create an instance Size of the root volume in gigabytes"
+   type        = number
+}
+variable "delete_on_termination" {
+  type        = bool
+  description =  "Whether the volume should be destroyed on instance termination"
+}
+
+variable "name" {
+   description = "Name to be used on EC2 instance created"
+   type        = string
+}
+
+variable "algorithm" {
+  type        = string
+  description = "The name of the algorithm to use for the key."
+}
+
+variable "rsa_bits" {
+  type        = number
+  description = "When algorithm is \"RSA\", the size of the generated RSA key in bits."
+}
+
+variable "key_name" {
+   type = string
+   description = "Key name of the Key Pair to use for the instance; which can be managed using the `aws_key_pair` resource"
+}
+
+
+variable "aws-region" {
+  type        = "string"
+  description =  "AWS Region the instance is launched in"
+  
+}
+
+
+
+
+
+
+
+
+
